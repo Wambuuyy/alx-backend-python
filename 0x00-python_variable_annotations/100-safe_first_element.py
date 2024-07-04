e0ffd5721ef90037b18bfae45893e6528ad3401e
Wambuuyy <wambuiprudy@gmail.com>
2024-07-04 17:51:25 +0800

    docum

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -5,6 +5,7 @@ from typing import Sequence, Any, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """safe first element"""
     if lst:
         return lst[0]
     else:
--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -4,4 +4,5 @@ from typing import List, Tuple, Iterable, Sequence
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """element length"""
     return [(i, len(i)) for i in lst]
