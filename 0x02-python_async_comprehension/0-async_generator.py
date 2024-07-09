be7aa0b3686e5d1b9c2a2153ea9c5d104092145f
Wambuuyy <wambuiprudy@gmail.com>
2024-07-09 13:53:32 +0300

    correct generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,10 +5,10 @@
 """
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """loops 10 times, each asynchronously wait 1 second and yield"""
     for _ in range(10):
         await asyncio.sleep(1)
